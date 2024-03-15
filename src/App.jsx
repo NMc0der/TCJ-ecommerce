67d01e2180d15fba1c2abe55eaa6bcb81fd5e399
Noah <noahglennmaldonado@gmail.com>
2024-03-15 13:18:20 -0400

    finished services section

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Hero from "./components/Hero";
 import "./App.css";
 import Category from "./components/Category";
 import Category2 from "./components/Category2";
+import Services from "./components/Sevices";
 
 function App() {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar />
       <Hero />
       <Category />
       <Category2 />
+      <Services />
     </div>
   );
 }
