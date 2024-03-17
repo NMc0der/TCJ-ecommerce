38cf5bdd3fa83ecc512c775b288c413b5c4d5810
Noah <noahglennmaldonado@gmail.com>
2024-03-17 11:45:34 -0400

    added products data and section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Category2 from "./components/Category2";
 import Services from "./components/Sevices";
 import Banner from "./components/Banner";
 import headphones from "./assets/headphone.png";
+import Products from "./components/Products";
+import smartwatch2 from "./assets/smartwatch2-removebg-preview.png";
 
 const BannerData = {
   discount: "30% OFF",
@@ -20,6 +22,18 @@ const BannerData = {
   bgColor: "#f42c37",
 };
 
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "14 Jan to 28 Jan",
+  image: smartwatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+  bgColor: "#2dcc6f",
+};
+
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
@@ -29,6 +43,8 @@ function App() {
       <Category2 />
       <Services />
       <Banner data={BannerData} />
+      <Products />
+      <Banner data={BannerData2} />
     </div>
   );
 }
